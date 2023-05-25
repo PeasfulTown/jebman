b420f7ad2155de2600b99b2ce05fedbf4bb233f8
PeasfulTown <peasfultown@gmail.com>
2023-05-24 20:10:18 -0400

    feat: add tag class

---
+++ src/main/java/xyz/peasfultown/domain/Tag.java
@@ -0,0 +1,55 @@
+package xyz.peasfultown.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class Tag {
+    private int id;
+    private String name;
+    public Tag(String name) {
+        this.id = 0;
+        this.name = name;
+    }
+
+    public Tag(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "," + this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        return new EqualsBuilder().append(id, tag.id).append(name, tag.name).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(id).append(name).toHashCode();
+    }
+}
